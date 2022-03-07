983e63bc587da10060de14b3d2109cd2795d9c93
Alper Kalaycioglu <alperkal@gmail.com>
2022-03-07 16:15:25 +0100

    Reqwork on params

--- gitcheck.sh
+++ gitcheck.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+echo "First Param: $1"
+echo "Second Param: $2"
 
-filename=`git diff --name-only HEAD^|head -1`
-commitMessage=`git log -1 --pretty=format:%B`
+filename=$1
+if [ -z "$filename" ]
+then
+    filename=`git diff --name-only HEAD^|head -1`
+fi
+
+commitMessage=$2
+if [ -z "$commitMessage" ]
+then
+    commitMessage=`git log -1 --pretty=format:%B`
+fi
+
 
 if [[ $filename == assets/img/* ]] && ([[ $filename == *jpg ]] || [[ $filename == *JPG ]]) ; then
     echo "True $commitMessage"
