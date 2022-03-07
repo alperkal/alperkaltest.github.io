e9796c10d82bbbe5e7b2cb6c44f6977b71661091
Alper Kalaycioglu <alperkal@gmail.com>
2022-03-07 14:08:22 +0100

    Modify scripts

---
+++ gitcheck.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+filename=`git diff --name-only HEAD\^|head -1`
+commitMessage=`git log -1 --pretty=format:%B`
+
+if [[ $filename == assets/img/* ]] && ([[ $filename == *jpg ]] || [[ $filename == *JPG ]]) ; then
+    echo "True $commitMessage"
+    ./newPost.sh $filename "$commitMessage"
+else
+    echo "false"
+fi
