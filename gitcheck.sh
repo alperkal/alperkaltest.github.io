e5c9971d59ecc36d4460c3e9721e2fc4c17325a5
Alper Kalaycioglu <alperkal@gmail.com>
2022-03-07 15:55:34 +0100

    Clean up

--- gitcheck.sh
+++ gitcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-filename=`git diff --name-only HEAD\^|head -1`
+filename=`git diff --name-only HEAD^|head -1`
 commitMessage=`git log -1 --pretty=format:%B`
 
 if [[ $filename == assets/img/* ]] && ([[ $filename == *jpg ]] || [[ $filename == *JPG ]]) ; then
